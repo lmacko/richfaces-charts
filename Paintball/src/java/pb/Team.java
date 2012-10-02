c7238e7c7ebcd1bbf10da5b35ba307dfa85b4f48
Macko <Macko@lukas-ThinkPad>
2012-10-02 17:09:32 +0200

    vyklikana crud app

---
+++ Paintball/src/java/pb/Team.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pb;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Macko
+ */
+@Entity
+@Table(name = "TEAM")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Team.findAll", query = "SELECT t FROM Team t"),
+    @NamedQuery(name = "Team.findById", query = "SELECT t FROM Team t WHERE t.id = :id"),
+    @NamedQuery(name = "Team.findByName", query = "SELECT t FROM Team t WHERE t.name = :name"),
+    @NamedQuery(name = "Team.findByMembercount", query = "SELECT t FROM Team t WHERE t.membercount = :membercount")})
+public class Team implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Size(max = 30)
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "MEMBERCOUNT")
+    private Integer membercount;
+    @OneToMany(mappedBy = "teamid1")
+    private Collection<Game> gameCollection;
+    @OneToMany(mappedBy = "teamid2")
+    private Collection<Game> gameCollection1;
+
+    public Team() {
+    }
+
+    public Team(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMembercount() {
+        return membercount;
+    }
+
+    public void setMembercount(Integer membercount) {
+        this.membercount = membercount;
+    }
+
+    @XmlTransient
+    public Collection<Game> getGameCollection() {
+        return gameCollection;
+    }
+
+    public void setGameCollection(Collection<Game> gameCollection) {
+        this.gameCollection = gameCollection;
+    }
+
+    @XmlTransient
+    public Collection<Game> getGameCollection1() {
+        return gameCollection1;
+    }
+
+    public void setGameCollection1(Collection<Game> gameCollection1) {
+        this.gameCollection1 = gameCollection1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Team)) {
+            return false;
+        }
+        Team other = (Team) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pb.Team[ id=" + id + " ]";
+    }
+    
+}
